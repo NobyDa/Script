ee912d81242bd83569816d4d525216cb4edf3b3e
NobyDa <53217160+NobyDa@users.noreply.github.com>
2019-07-29 12:07:20 +0800

    Update zymk.js

--- QuantumultX/File/zymk.js
+++ QuantumultX/File/zymk.js
@@ -1,27 +1,31 @@
 let user = '/app_api/v5/getuserinfo/';
 let coin = '/app_api/v5/coin_account/';
 let ticket = '/app_api/v5/getuserinfo_ticket/';
+let free = '/app_api/v5/getcomicinfo/';
 
 let url = $request.url;
 let body = JSON.parse($response.body);
 
 if (url.indexOf(user) != -1) {
-	body.data.coins = 999;
+	body.data.coins = 6666;
 	body.data.isvip = 1;
-	body.data.recommend = 999;
-	body.data.Cticket = 999;
-	body.data.Cgold = 999;
-} else if (url.indexOf(coin) != -1) {
-	body.data.coins = 999;
-	body.data.golds = 999;
-} else if (url.indexOf(ticket) != -1) {
-	body.data.Cticket = 999;	
-} else {
-//    $done({});
+	body.data.recommend = 6666;
+	body.data.Cticket = 6666;
+	body.data.Cgold = 6666;
+}
+else if (url.indexOf(coin) != -1) {
+	body.data.coins = 6666;
+	body.data.golds = 6666;
+}
+else if (url.indexOf(ticket) != -1) {
+	body.data.Cticket = 6666;	
+} 
+
+else if (url.indexOf(free) != -1) {
+	body.data.price = 0;	
+	body.data.download_price = 0;	
 }
 
 body = JSON.stringify(body);
 
 $done({body});
-
-// By mieqq
