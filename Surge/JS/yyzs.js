a0ec0a76f04dd8d851e60ee10de5bed3e6e51011
NobyDa <NobyDa@users.noreply.github.com>
2019-08-28 18:59:38 +0800

    Update yyzs.js
    Medication Assistant Unlock Professional Edition

--- Surge/JS/yyzs.js
+++ Surge/JS/yyzs.js
@@ -1,6 +1,8 @@
 /*
+Surge:
+
 [Script]
-http-response ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat\?|init\?timestamp=)) requires-body=1,max-size=0,script-path=DingxiangDrugs.js
+http-response ^https?:\/\/(i|newdrugs)\.dxy\.cn\/(snsapi\/username\/|app\/user\/(pro\/stat\?|init\?timestamp=)) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/yyzs.js
 
 [MITM]
 hostname = newdrugs.dxy.cn
