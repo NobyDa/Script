04ac609283ca8dfbbf5e3e903048ace9883a91d9
NobyDa <NobyDa@users.noreply.github.com>
2019-08-31 03:25:11 +0800

    Update Kuwo.js

--- Surge/JS/Kuwo.js
+++ Surge/JS/Kuwo.js
@@ -12,10 +12,10 @@ const vip = '/vip/v2/user/vip';
 const time = '/vip/spi/mservice';
 
 if (url.indexOf(vip) != -1) {
-	obj.data["isNewUser"] = "1";
+	obj.data["isNewUser"] = "2";
 	obj.data["vipLuxuryExpire"] = "1835312949000";
 	obj.data["time"] = "1961170340993";
-	obj.data["isYearUser"] = "1";
+	obj.data["isYearUser"] = "2";
 	obj.data["vipmExpire"] = "1835312949000";
 	obj.data["vipOverSeasExpire"] = "1835312949000";
 	obj.data["vipExpire"] = "1835312949000";
@@ -24,8 +24,8 @@ if (url.indexOf(vip) != -1) {
 }
 
 if (url.indexOf(time) != -1) {
-	obj["isVIPMAutoPay"] = 1;
-	obj["isVIPLuxAutoPay"] = 1;
+	obj["isVIPMAutoPay"] = 2;
+	obj["isVIPLuxAutoPay"] = 2;
 	body = JSON.stringify(obj);
 }
 
