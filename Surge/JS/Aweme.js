4cafded35c24b30e2bbbb6db7abfdf0a601887c1
NobyDa <NobyDa@users.noreply.github.com>
2019-09-15 08:42:11 +0800

    Add

--- Surge/JS/Aweme.js
+++ Surge/JS/Aweme.js
@@ -1,16 +1,27 @@
 /*
+[URL Rewrite]
+^https://aweme-eagle(.*)\.snssdk\.com/aweme/v2/ https://aweme-eagle$1.snssdk.com/aweme/v1/ 302
+
 [Script]
-http-response ^https://[\s\S]*\.snssdk\.com/.+/(feed|post)/ requires-body=1,max-size=-1,script-path=https://Choler.github.io/Surge/Script/Aweme.js
+http-response ^https://[\s\S]*\/aweme/v1/(feed|aweme/post|follow/feed)/ requires-body=1,max-size=-1,script-path=https://Choler.github.io/Surge/Script/Aweme.js
 
 [MITM]
-hostname = *.snssdk.com
+hostname = *.amemv.com, *.snssdk.com
 */
 
+let arr = {
+  "allow_download": true,
+  "share_type": 0,
+  "show_progress_bar": 0,
+  "draft_progress_bar": 0,
+  "allow_duet": true,
+  "allow_react": true,
+  "prevent_download_type": 2,
+  "allow_dynamic_wallpaper": false
+};
 let body = $response.body.replace(/watermark=1/g, "watermark=0");
 var obj = JSON.parse(body);
-if ($request.url.indexOf("life") > 0) {
-  $done({});
-} else if (obj.aweme_list) {
+if (obj.aweme_list) {
   for (var i = obj.aweme_list.length - 1; i >= 0; i--) {
     if (obj.aweme_list[i].raw_ad_data) {
       obj.aweme_list.splice(i, 1);
@@ -24,13 +35,8 @@ if ($request.url.indexOf("life") > 0) {
     if (obj.aweme_list[i].simple_promotions) {
       delete obj.aweme_list[i].simple_promotions;
     }
-    if (obj.aweme_list[i].interaction_stickers != null) {
-      obj.aweme_list[i].interaction_stickers = null;
-    }
-    if (obj.aweme_list[i].video_control.allow_download != true) {
-      obj.aweme_list[i].status.reviewed = 1;
-      obj.aweme_list[i].video_control.allow_download = true;
-    }
+    obj.aweme_list[i].status.reviewed = 1;
+    obj.aweme_list[i].video_control = arr;
   }
   $done({body: JSON.stringify(obj)});
 } else if (obj.data) {
@@ -42,10 +48,8 @@ if ($request.url.indexOf("life") > 0) {
       if (obj.data[i].aweme.simple_promotions) {
         delete obj.data[i].aweme.simple_promotions;
       }
-      if (obj.data[i].aweme.video_control.allow_download != true) {
-        obj.data[i].aweme.status.reviewed = 1;
-        obj.data[i].aweme.video_control.allow_download = true;
-      }
+      obj.data[i].aweme.status.reviewed = 1;
+      obj.data[i].aweme.video_control = arr;
     } else {
       obj.data.splice(i, 1);
     }
