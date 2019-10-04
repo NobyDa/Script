aaf8cda4b0cc0aa6af53e495f6265783bc2c5884
NobyDa <53217160+NobyDa@users.noreply.github.com>
2019-10-05 00:30:52 +0800

    Update honey peach comic

--- QuantumultX/File/bdmh.js
+++ QuantumultX/File/bdmh.js
@@ -8,7 +8,7 @@ obj.data.user_info.vip_days = "66666";
 obj.data.user_info.vip_start_time = "1502969604";
 obj.data.user_info.vip_overtime = "2066-06-06 06:00:00";
 obj.data.user_info.name = "脚本禁止牟利,TG频道@NobyDa";
-obj.data.user_info.avatar = "https://p1.music.126.net/1jOx2v1wy36uXNh9lXR1Fg==/109951163188414241.jpg?param=180y180";
+obj.data.user_info.avatar = "https://avatars3.githubusercontent.com/u/53217160?s=400&v=4";
 
 body = JSON.stringify(obj); 
 $done({body});
