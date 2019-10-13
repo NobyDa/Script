ad25c4f82212528e391cd9d1dd99b46a5ec41b18
NobyDa <53217160+NobyDa@users.noreply.github.com>
2019-10-13 23:02:52 +0800

    Fix

--- QuantumultX/File/bilibiliAccount.js
+++ QuantumultX/File/bilibiliAccount.js
@@ -1,6 +1,5 @@
 let body = $response.body
 body=JSON.parse(body)
-body['data']['sections'].splice(0,1)
-body['data']['sections'].splice(1,1)
+body['data']['sections'].splice(2,1)
 body=JSON.stringify(body)
 $done({body})
