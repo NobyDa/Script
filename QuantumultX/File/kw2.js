6a189cbb0ae9bed73b0fe8a582e66e7543ba788f
NobyDa <53217160+NobyDa@users.noreply.github.com>
2019-10-22 20:53:30 +0800

    Add tick list, Fix pumpkin

--- QuantumultX/File/kw2.js
+++
@@ -1,7 +0,0 @@
-var body = $response.body;
-var obj = JSON.parse(body);
-
-obj["isVIPMAutoPay"] = 2;
-obj["isVIPLuxAutoPay"] = 2;
-body = JSON.stringify(obj);
-$done(body);
--- Surge/JS/ngdy.js
+++ Surge/JS/ngdy.js
@@ -9,29 +9,4 @@ QX:
 Surge & QX MITM = pay.guoing.com, p.doras.api.vcinema.cn,
 */
 
-let obj = JSON.parse($response.body);
-let url = $request.url;
-let photo = "https://avatars2.githubusercontent.com/u/53217160";
-let name = "禁止牟利, TG频道@NobyDa";
-const vip = 'v5.0/user';
-const pay = 'user/get_user_info';
-
-　if (url.indexOf(vip) != -1) {
-　　obj.content["user_seed_int"] = "6666666";
-　　obj.content["user_photo"] = photo;
-　　obj.content["user_id"] = "12873551";
-　　obj.content["user_phone"] = "16666666666";
-　　obj.content["user_level_str"] = "Lv5男爵";
-　　obj.content["user_vip_state"] = "2";
-　　obj.content["user_nickname"] = name;
-　　obj.content["user_vip_start_date"] = "2016.06.06";
-　　obj.content["user_vip_end_date"] = "2066.06.06";
-}
-
-　if (url.indexOf(pay) != -1) {
-　　obj.content["user_vip_end_date"] = "2066-06-06";
-　　obj.content["user_phone"] = "16666666666";
-　　obj.content["user_nickname"] = name;
-　　obj.content["user_photo"] = photo;
-}
-$done({body: JSON.stringify(obj)});
+var _0xf504=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x75\x72\x6C","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x76\x61\x74\x61\x72\x73\x32\x2E\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6F\x6E\x74\x65\x6E\x74\x2E\x63\x6F\x6D\x2F\x75\x2F\x35\x33\x32\x31\x37\x31\x36\x30","\u7981\u6B62\u725F\u5229\x2C\x20\u6570\u636E\u6765\u81EA\x40\u4F59\u6E29","\x76\x35\x2E\x30\x2F\x75\x73\x65\x72","\x75\x73\x65\x72\x2F\x67\x65\x74\x5F\x75\x73\x65\x72\x5F\x69\x6E\x66\x6F","\x69\x6E\x64\x65\x78\x4F\x66","\x75\x73\x65\x72\x5F\x73\x65\x65\x64\x5F\x69\x6E\x74","\x63\x6F\x6E\x74\x65\x6E\x74","\x36\x36\x36\x36\x36\x36\x36","\x75\x73\x65\x72\x5F\x70\x68\x6F\x74\x6F","\x75\x73\x65\x72\x5F\x69\x64","\x31\x37\x34\x35\x35\x32\x39\x36","\x75\x73\x65\x72\x5F\x70\x68\x6F\x6E\x65","\x31\x36\x36\x36\x36\x36\x36\x36\x36\x36\x36","\x75\x73\x65\x72\x5F\x6C\x65\x76\x65\x6C\x5F\x73\x74\x72","\x4C\x76\x35\u7537\u7235","\x75\x73\x65\x72\x5F\x76\x69\x70\x5F\x73\x74\x61\x74\x65","\x32","\x75\x73\x65\x72\x5F\x6E\x69\x63\x6B\x6E\x61\x6D\x65","\x75\x73\x65\x72\x5F\x76\x69\x70\x5F\x73\x74\x61\x72\x74\x5F\x64\x61\x74\x65","\x32\x30\x31\x36\x2E\x30\x36\x2E\x30\x36","\x75\x73\x65\x72\x5F\x76\x69\x70\x5F\x65\x6E\x64\x5F\x64\x61\x74\x65","\x32\x30\x36\x36\x2E\x30\x36\x2E\x30\x36","\x32\x30\x36\x36\x2D\x30\x36\x2D\x30\x36","\x73\x74\x72\x69\x6E\x67\x69\x66\x79"];let obj=JSON[_0xf504[1]]($response[_0xf504[0]]);let url=$request[_0xf504[2]];let photo=_0xf504[3];let name=_0xf504[4];const vip=_0xf504[5];const pay=_0xf504[6];　if(url[_0xf504[7]](vip)!=  -1);{　　obj[_0xf504[9]][_0xf504[8]]= _0xf504[10];　　obj[_0xf504[9]][_0xf504[11]]= photo;　　obj[_0xf504[9]][_0xf504[12]]= _0xf504[13];　　obj[_0xf504[9]][_0xf504[14]]= _0xf504[15];　　obj[_0xf504[9]][_0xf504[16]]= _0xf504[17];　　obj[_0xf504[9]][_0xf504[18]]= _0xf504[19];　　obj[_0xf504[9]][_0xf504[20]]= name;　　obj[_0xf504[9]][_0xf504[21]]= _0xf504[22];　　obj[_0xf504[9]][_0xf504[23]]= _0xf504[24]}　if(url[_0xf504[7]](pay)!=  -1);{　　obj[_0xf504[9]][_0xf504[23]]= _0xf504[25];　　obj[_0xf504[9]][_0xf504[14]]= _0xf504[15];　　obj[_0xf504[9]][_0xf504[20]]= name;　　obj[_0xf504[9]][_0xf504[11]]= photo}$done({body:JSON[_0xf504[26]](obj)})
