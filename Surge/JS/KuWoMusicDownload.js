8749dea4264c048150df1adac0fc7ee9ee540263
humingxNobyDaconfig --global user.email 53217160+NobyDa@users.noreply.github.comssh-keygen -t rsa -C humingx@yeah.nethumingx@yeah.net <53217160+NobyDa@users.noreply.github.com>
2019-11-22 11:54:06 +0800

    Add vip script

--- Surge/JS/KuWoMusicDownload.js
+++ Surge/JS/KuWoMusicDownload.js
@@ -3,6 +3,9 @@ KuWo music unlock lossless download, need to use with KuWo music VIP script. thi
 
 Surge4.0:
 http-request https?:\/\/musicpay\.kuwo.cn\/music\.pay\?uid=\d+ script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/KuWoMusicDownload.js
+#Vip script:
+http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
+
 */
 
 let url = $request.url.replace(/uid=\d+/g, "uid=1");
