596ebee163c34315a0c37a2d64c10e26dcaf348c
Julia <53378607+limbopro@users.noreply.github.com>
2019-11-23 01:27:29 +0800

    Update Termius.js

--- Surge/JS/Termius.js
+++ Surge/JS/Termius.js
@@ -11,6 +11,10 @@ https:\/\/api\.termius\.com\/api\/v3\/bulk\/account\/ url script-response-body h
 Surge & QX MITM = api.termius.com
 */
 
+/**
+ * @supported 91084FFA2796
+ */
+
 let obj=JSON.parse($response.body)
 obj.account["pro_mode"] = true;
 obj.account["plan_type"] = "Premium";
