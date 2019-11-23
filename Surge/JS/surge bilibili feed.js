6da91baf79819c2a03f4922557234fa5bf0c052e
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2019-11-23 17:40:09 +0800

    Update surge bilibili feed.js

--- Surge/JS/surge bilibili feed.js
+++ Surge/JS/surge bilibili feed.js
@@ -1,4 +1,6 @@
-
+/*
+ * @supported 0C02A0BCC5B7
+ */
 //Customize blacklist
 let blacklist=['共青团中央','广东共青团','浙江共青团','山东共青团','安徽共青团','河南共青团','央视频','徐大sao','翔翔大作战','徐大虾咯','科技美学','敬汉卿','NathanRich火锅大王','千户长生']
 
