5b980c866e36fb0d8c42b20ab8c45d76598b0002
i666z-666 <58083743+i666z-666@users.noreply.github.com>
2019-11-23 09:05:16 +0800

    Update aimeiju.js

--- QuantumultX/File/aimeiju.js
+++ QuantumultX/File/aimeiju.js
@@ -9,6 +9,10 @@ Surge4：
 http-response ^https?:\/\/mjapp\.\w{1,9}\.com\/index\.php\/app\/ios\/(vod\/show|user\/index) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/aimeiju.js
 */
 
+/**
+ * @supported 68F7E880141E
+ */
+
 var body = $response.body;
 var url = $request.url;
 const path1 = "/index.php/app/ios/user/index";
