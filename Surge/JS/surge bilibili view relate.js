66078ffbecdbd937d085629844bef3a3007f2e1a
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2019-11-23 17:34:20 +0800

    Create surge bilibili view relate.js

---
+++ Surge/JS/surge bilibili view relate.js
@@ -0,0 +1,14 @@
+/*
+ * @supported 0C02A0BCC5B7
+ */
+
+let body = $response.body
+body=JSON.parse(body)
+body['data']['relates'].forEach((element, index)=> {
+   if(element.hasOwnProperty('is_ad')||!element.hasOwnProperty('aid')){      
+      body['data']['relates'].splice(index,1)  
+    }
+})
+delete body['data']['cms']
+body=JSON.stringify(body)
+$done({body})
