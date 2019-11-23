04e9e5f4eae8d47a57eb8f395b55a45ac705e3e2
i666z-666 <58083743+i666z-666@users.noreply.github.com>
2019-11-23 09:06:10 +0800

    Update bilibiliAccount.js

--- QuantumultX/File/bilibiliAccount.js
+++ QuantumultX/File/bilibiliAccount.js
@@ -10,6 +10,10 @@ http-response ^https://app.bilibili.com/x/v2/account/mine\?access_key requires-b
 Surge & QX MITM = app.bilibili.com
 */
 
+/**
+ * @supported 68F7E880141E
+ */
+
 let body = $response.body
 body=JSON.parse(body)
 body['data']['sections'].splice(2,1)
