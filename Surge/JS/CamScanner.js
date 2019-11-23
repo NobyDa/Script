1084affaa94d0dc31481b093a5a09e247073ca8a
i666z-666 <58083743+i666z-666@users.noreply.github.com>
2019-11-23 09:44:34 +0800

    Update CamScanner.js

--- Surge/JS/CamScanner.js
+++ Surge/JS/CamScanner.js
@@ -13,6 +13,10 @@ http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\
 QX & Q & Surge Mitm = ap*.intsig.net,
 */
 
+/**
+ * @supported 68F7E880141E
+ */
+
 let obj = JSON.parse($response.body);
 obj = {"data":{"psnl_vip_property":{"expiry":"1643731200"}}};
 $done({body: JSON.stringify(obj)});
