2c47707cc35bb1b958296a4f7952f00976a57660
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2019-11-23 17:32:45 +0800

    Create surge bilibili space.js

---
+++ Surge/JS/surge bilibili space.js
@@ -0,0 +1,25 @@
+/*
+ * @supported 0C02A0BCC5B7
+ */
+
+
+let url = $request.url
+let regex = /vmid=(\d*)/
+let vmid= regex.exec(url)
+let mid = vmid[1]
+let api = `https://space.bilibili.com/ajax/member/getSubmitVideos?mid=${mid}&pagesize=10&order=stow`
+$httpClient.get(api, (error, response, body) => {
+    if (error) $done({})
+    else {
+      console.log(body)
+      body=JSON.parse(body)
+      let info=""
+      body['data']['vlist'].forEach((element, index)=> {
+          index++
+          let scheme=`bilibili://av/${element['aid']}`
+          info+=index+": "+element['title']+"\n"+scheme+"\n"
+      })     
+      $notification.post('收藏排行前10','长按进入', info)  
+      $done({})
+    }
+  })
