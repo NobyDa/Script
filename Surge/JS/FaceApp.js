5ea3660b00fa93834006c94ca0b2f038be49e39e
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2019-11-23 14:03:59 +0800

    Update FaceApp.js

--- Surge/JS/FaceApp.js
+++ Surge/JS/FaceApp.js
@@ -1,4 +1,8 @@
 /*
+ * @supported 0C02A0BCC5B7
+ */
+
+/*
 [Script]
 http-response ^https?:\/\/api\.faceapp\.io(.*)\/api\/v3.0\/auth\/user\/credentials requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/FaceApp.js
 
