447e66381a6dcfb3a917e2672246e50dce8bcc3e
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2019-11-23 17:33:30 +0800

    Create surge bilibili tab.js

---
+++ Surge/JS/surge bilibili tab.js
@@ -0,0 +1,23 @@
+/*
+ * @supported 0C02A0BCC5B7
+ */
+
+//Customize whitelist
+let whitelist=['追番','推荐','直播','热门','影视']
+
+let body = $response.body
+body=JSON.parse(body)
+
+body['data']['tab'].forEach((element, index) => {
+if(!(whitelist.includes(element['name']))) body['data']['tab'].splice(index,1)  
+});
+
+body['data']['bottom'].forEach((element, index)=> {
+    if(element['pos']==4){      
+       body['data']['bottom'].splice(index,1)  
+    }
+})
+
+delete body['data']['top']
+body=JSON.stringify(body)
+$done({body}) 
