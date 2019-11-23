1823d3c4dd20480d6f77cac63cc63608974ea69b
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2019-11-23 14:05:14 +0800

    Update wxzb.js

--- Surge/JS/wxzb.js
+++ Surge/JS/wxzb.js
@@ -1,4 +1,8 @@
 /*
+ * @supported 0C02A0BCC5B7
+ */
+
+/*
 Vientiane live download link：https://m.51168.tv/
 Surge4：
 http-response https:\/\/u\.kanghuayun\.com\/api\/v2\/info requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/wxzb.js
