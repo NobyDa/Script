1c883d73fcc126353b4a74d552b5a7db9fafa504
eR1zxNO <31590640+eR1zxNO@users.noreply.github.com>
2019-11-23 22:16:54 +0800

    Update syjl.js

--- Surge/JS/syjl.js
+++ Surge/JS/syjl.js
@@ -13,3 +13,7 @@ let obj = JSON.parse($response.body);
 obj.data["is_vip"] = 1;
 obj.data["vip_end_time"] = "2066-06-06";
 $done({body: JSON.stringify(obj)});
+
+/**
+ * @supported 8CAAE3BAA2B1
+ */
