751bac5baca09cc40916cab70b07275c883a0036
limbopro <service.limbopro.xyz@gmail.com>
2019-11-26 02:44:47 +0800

    清空

--- QuantumultX/1.0.3-remote.js
+++
@@ -1,4 +0,0 @@
-var body = $response.body;
-body = '\/*\n@supported 91084FFA2796\n*\/\n' + body;
-
-$done(body);
--- Surge/JS/luqi.js
+++
@@ -1,4 +0,0 @@
-let obj = JSON.parse($response.body);
-obj.data.is_vip = 1;
-obj.data.vip_endtime = 1630296877;
-$done({body: JSON.stringify(obj)});
