c1b650c0ad8da914e77203a0b22e5bb74e4c0afd
Julia <53378607+limbopro@users.noreply.github.com>
2019-11-26 18:06:36 +0800

    Update Termius.js

--- Surge/JS/Termius.js
+++ Surge/JS/Termius.js
@@ -15,5 +15,5 @@ let obj=JSON.parse($response.body)
 obj.account["pro_mode"] = true;
 obj.account["plan_type"] = "Premium";
 obj.account["user_type"] = "Premium";
-obj.account["current_period"]["until"] = "2199-10-10T03:27:34";
+obj.account["current_period"]["until"] = "2299-10-10T03:27:34";
 $done({body:JSON.stringify(obj)})
