b40d9e1ab907833316e8e2b4abf096e4403e2873
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2019-12-02 00:53:15 +0800

    Update wps.js

--- Surge/wps.js
+++ Surge/wps.js
@@ -1,4 +1,5 @@
 /*
+@supported 819366A72B49
 国区wps
 [Script]
 http-response ^https?:\/\/account\.wps\.cn\/api\/users requires-body=1,max-size=-1,script-path=https://raw.githubusercontent.com/eHpo1/Surge/master/Script/wps.js
