76492cfd8afb9deb842f98a53c46625e983f8377
songyangzz <623984375@qq.com>
2019-12-02 17:56:36 +0800

    Update xxys.js

--- QuantumultX/File/xxys.js
+++ QuantumultX/File/xxys.js
@@ -4,9 +4,9 @@ QX:
 https:\/\/ios\.xiaoxiaoapps\.com\/(vod\/reqplay\/|ucp/index) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
 Surge:
 http-response https:\/\/ios\.xiaoxiaoapps\.com\/(vod\/reqplay\/|ucp/index) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/xxys.js
-
 MITM = ios.xiaoxiaoapps.com
 */
+
 const path1 = "/ucp/index";
 const path2 = "/vod/reqplay/";
 var body = $response.body;
@@ -28,14 +28,11 @@ if (url.indexOf(path2) != -1){
 	let obj = JSON.parse(body); 
 	obj.retcode = "0";
 	obj.data.lastplayindex = "1";
-
 	if(obj.data.hasOwnProperty("httpurl_preview")){
 		var playurl = obj.data["httpurl_preview"];
 		obj.data["httpurl"] = playurl;
 	};
-
 	body = JSON.stringify(obj);
 }
 
 $done({body});
-
