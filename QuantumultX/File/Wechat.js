650bfe6a7123fcb3dab0c565c4cff585e30f5ce3
alpha87 <jianxun2004@gmail.com>
2019-12-03 10:55:19 +0800

    删除广告边框

--- QuantumultX/File/Wechat.js
+++ QuantumultX/File/Wechat.js
@@ -13,6 +13,7 @@ Surge & QX MITM = mp.weixin.qq.com,
 var obj = JSON.parse($response.body);
 obj.advertisement_num = 0;
 obj.advertisement_info = [];
+delete obj.appid;
 $done({body: JSON.stringify(obj)}); 
 
 //by Choler
