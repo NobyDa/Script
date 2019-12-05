d98ca2478b03ea9cd91904a08985d13559e68454
songyangzz <623984375@qq.com>
2019-12-05 10:57:15 +0800

    Update tik.js

--- QuantumultX/File/tik.js
+++ QuantumultX/File/tik.js
@@ -1,1 +1,10 @@
-111
+var body = $response.header;
+var url = $request.url;
+const path = "/app_api/v5/getuserinfo/";
+let obj = JSON.parse(body);
+if (url.indexOf(path) != -1) {
+	obj["isvip"] = "1";
+	body = JSON.stringify(obj);
+ }
+obj[x-common-params-v2]="test";
+$done({body});
