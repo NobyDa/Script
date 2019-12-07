0c2534db477e09330685e0b3f4d1067836348f0b
NobyDa <53217160+NobyDa@users.noreply.github.com>
2019-12-07 19:32:57 +0800

    put right

--- Surge/JS/FaceApp.js
+++ Surge/JS/FaceApp.js
@@ -6,19 +6,15 @@ http-response ^https?:\/\/api\.faceapp\.io(.*)\/api\/v.*\/auth\/user\/credential
 hostname = api.faceapp.io
 */
 
-    let headers = $response.headers;
-    let status = $response.status;
-    let obj = JSON.parse($response.body);
+let headers = $response.headers; 
+let obj = JSON.parse($response.body);
+ 
+obj = {"subscription_apple":{"subscription_exp":3042979200,"product_id":"p"}};  
 
-    {
-    obj = {"subscription_apple":{"subscription_exp":3042979200,"product_id":"y"},"subscription_google":null};
-    $done({body: JSON.stringify(obj)});
-}
-    {
-    status = 200;
-    $done({status});
-}
-    {
-    delete headers['X-FaceApp-ErrorCode'];
-    $done({headers});
-}
+delete headers['X-FaceApp-ErrorCode'];
+ 
+$done({
+    body: JSON.stringify(obj),
+    headers: headers,
+    status: 200,
+});
