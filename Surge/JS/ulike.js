5cc68dffd7c36a0dc3f4d4c18bba05fa7d7951a2
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2020-01-16 01:00:09 +0800

    Update ulike.js

--- Surge/JS/ulike.js
+++ Surge/JS/ulike.js
@@ -1,20 +1,17 @@
-var body = $response.body;
 let obj = JSON.parse($response.body);
-obj= {
-	“ret":"0",
-	"errmsg":"Success",
-	"systime":"",
-	"data":{
-		"flag":true,
-		"start_time":1579094492,
-		"end_time":3725012184,
-		"is_first_subscribe":false,
-		"is_cancel_subscribe":false,
-		"uid":"4089778978429341",
-		"subscribe_uid":"70851259049"
-		    }
-	}
-body = JSON.stringify(obj);
-$done({body});
-
+obj ={
+"ret":"0",
+"errmsg":"Success",
+"systime":"",
+"data":{
+"flag":true,
+"start_time":1579094492,
+"end_time":3725012184,
+"is_first_subscribe":false,
+"is_cancel_subscribe":false,
+"uid":"4089778978429341",
+"subscribe_uid":"70851259049"
+}
+};
+$done({body: JSON.stringify(obj)});
 
