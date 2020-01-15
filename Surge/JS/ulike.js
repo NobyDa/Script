d07a70825208c880b55d70ba6a62781c9e9108c9
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2020-01-15 23:11:07 +0800

    Create ulike.js

---
+++ Surge/JS/ulike.js
@@ -0,0 +1,15 @@
+let obj = JSON.parse($response.body);
+obj= {
+	“ret":"0",
+	"errmsg":"Success",
+	"systime":"",
+	"data":{
+		"flag":true,
+		"start_time":1579094492,
+		"end_time":3725012184,
+		"is_first_subscribe":false,
+		"is_cancel_subscribe":false,
+		"uid":"4089778978429341",
+		"subscribe_uid":"70851259049"
+		    }
+	}
