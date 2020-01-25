04911484cc6f102be29f27860b7e3266aa39396d
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2020-01-25 14:04:28 +0800

    Create elemeCookie.js

---
+++ Surge/elemeCookie.js
@@ -0,0 +1,55 @@
+const cookieName = '饿了么'
+const cookieKey = 'cookie_elem'
+const sy = init()
+const cookieVal = $response.body;
+sy.log('${cookieVal}')
+if (cookieVal) {
+  if (sy.setdata(cookieVal, cookieKey)) {
+    sy.msg(`${cookieName}`, '获取Cookie: 成功', '')
+    sy.log(`[${cookieName}] 获取Cookie: 成功, cookie: ${cookieVal}`)
+  }
+}
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+sy.done() 
