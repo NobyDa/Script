af47d38199281b8bec00b5ed7896c249bec37712
velazquez111 <40903820+velazquez111@users.noreply.github.com>
2020-02-04 07:12:57 +0800

    Create doc.js

---
+++ Surge/JS/doc.js
@@ -0,0 +1,14 @@
+var obj= {
+  "originalTransactionId" : "20000625420102",
+  "subscriptionState" : "trial",
+  "isInGracePeriod" : false,
+  "subscriptionExpirationDate" : "17:48 25/11/2099",
+  "isDocuments6User" : true,
+  "isEligibleForIntroPeriod" : false,
+  "subscriptionAutoRenewStatus" : "autoRenewOff",
+  "subscriptionReceiptId" : "1530908572000"
+};
+
+$done({body: JSON.stringify(obj)});
+
+// Descriptions license.pdfexpert.com
