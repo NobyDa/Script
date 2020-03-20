568dc56a4e6c9a1c65103419f7a92ba5231d0836
humingxNobyDaconfig --global user.email 53217160+NobyDa@users.noreply.github.comssh-keygen -t rsa -C humingx@yeah.nethumingx@yeah.net <53217160+NobyDa@users.noreply.github.com>
2020-03-20 18:58:54 +0800

    enable valid checkin

--- JD-DailyBonus/JD_DailyBonus.js
+++ JD-DailyBonus/JD_DailyBonus.js
@@ -1,8 +1,8 @@
 /*
 京东多合一签到脚本
 
-更新于: 2020.3.19 20:50 v84
-有效接口: 22
+更新于: 2020.3.20 19:00 v84.1
+有效接口: 23
 
 该脚本同时兼容: QuantumultX, Surge, Loon, JSBox, Node.js
 如使用JSBox 或 Nodejs, 请自行抓取Cookie填入脚本Key处.
@@ -81,7 +81,7 @@ async function all() {//签到模块相互独立,您可注释某一行以禁用�
   await JingDongCash(stop); //京东现金红包
   await JingDongShoes(stop); //京东鞋靴馆
   await JingDongFood(stop); //京东美食馆
-  //await JingRSeeAds(stop); //金融看广告
+  await JingRSeeAds(stop); //金融看广告
   await JingRongGame(stop); //金融游戏大厅
   await JingDongLive(stop); //京东智能生活馆
   await JingDongClean(stop); //京东清洁馆
@@ -1447,10 +1447,10 @@ function JingRSeeAds(s) {
                 if (data.match(/(\"resultCode\":3|先登录)/)) {
                   merge.JRSeeAds.notify = "京东金融-广告: 失败, 原因: Cookie失效‼️"
                   merge.JRSeeAds.fail = 1
-                } else {
-                  merge.JRSeeAds.notify = "京东金融-广告: 失败, 原因: 未知 ⚠️"
-                  merge.JRSeeAds.fail = 1
-                }
+                } //else {
+                  //merge.JRSeeAds.notify = "京东金融-广告: 失败, 原因: 未知 ⚠️"
+                  //merge.JRSeeAds.fail = 1
+                //}
               }
             }
           }
