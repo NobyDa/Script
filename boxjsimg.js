a60dda517f1e53f744b3726858bec15be97fb6b9
Xirou <54771724+Xirou@users.noreply.github.com>
2020-07-31 18:19:37 +0800

    Update and rename boxjsimg.js to music.js

--- boxjsimg.js
+++
@@ -1,6 +0,0 @@
-<v-container fluid v-if="ui.curview === 'home'">
-              <v-img class="mb-4" src="https://api.neweb.top/bing.php"></v-img>
-              <v-img class="mb-4" src="http://api.btstu.cn/sjbz/?lx=m_dongman"></v-img>
-              <v-img class="mb-4" src="http://api.btstu.cn/sjbz/?lx=m_meizi"></v-img>
-              <v-img class="mb-4" src="http://api.btstu.cn/sjbz/?m_lx=suiji"></v-img>
-            </v-container>
---
+++ music.js
@@ -0,0 +1,3 @@
+shadowsocks=music.desperadoj.com:30003, method=aes-128-gcm, password=desperadoj.com_free_proxy_doe3, fast-open=false, udp-relay=false, tag=🎸
+http=taron.top:100,fast-open=false, udp-relay=false, tag=🎯
+http=106.52.127.72:19951,fast-open=false, udp-relay=false, tag=🎹
