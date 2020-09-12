915a087bab7d98190f1a2e3298210b81e2d7bcfe
NobyDa <53217160+NobyDa@users.noreply.github.com>
2020-09-12 13:59:29 +0800

    52pojie: 添加node,js支持 #198

--- 52pojie-DailyBonus/52pojie.js
+++ 52pojie-DailyBonus/52pojie.js
@@ -1,18 +1,30 @@
 /*
 吾爱破解签到脚本
 
-更新时间: 2020.8.21
-脚本兼容: QuantumultX, Surge, Loon
+更新时间: 2020.9.12
+脚本兼容: QuantumultX, Surge, Loon, Node.js
 电报频道: @NobyDa
 问题反馈: @NobyDa_bot
 
-说明：
+************************
+QX, Surge, Loon说明：
+************************
 手动登录 https://www.52pojie.cn 如通知成功获取cookie, 则可以使用此签到脚本.
-获取Cookie后, 请将Cookie脚本禁用并移除主机名，以免产生不必要的MITM.
-
-脚本将在每天上午9点执行。 您可以修改执行时间。
+获取Cookie后, 请将Cookie脚本禁用并移除主机名, 以免产生不必要的MITM.
+脚本将在每天上午9点执行, 您可以修改执行时间.
 
 ************************
+Node.js说明: 
+************************
+需自行安装"got"与"tough-cookie"模块. 例: npm install got tough-cookie -g
+
+抓取Cookie说明:
+浏览器打开 https://www.52pojie.cn/home.php 登录账号后, 开启抓包软件并刷新页面.
+抓取该URL请求头下的Cookie字段, 填入以下CookieWA的单引号内即可. */
+
+const CookieWA = '';
+
+/***********************
 Surge 4.2.0+ 脚本配置:
 ************************
 
@@ -25,16 +37,16 @@ Surge 4.2.0+ 脚本配置:
 hostname= www.52pojie.cn
 
 ************************
-QuantumultX 本地脚本配置:
+QuantumultX 远程脚本配置:
 ************************
 
 [task_local]
 # 吾爱签到
-0 9 * * * 52pojie.js
+0 9 * * * https://raw.githubusercontent.com/NobyDa/Script/master/52pojie-DailyBonus/52pojie.js
 
 [rewrite_local]
 # 获取Cookie
-https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header 52pojie.js
+https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/NobyDa/Script/master/52pojie-DailyBonus/52pojie.js
 
 [mitm] 
 hostname= www.52pojie.cn
@@ -54,37 +66,36 @@ http-request https:\/\/www\.52pojie\.cn\/home\.php\? script-path=https://raw.git
 hostname= www.52pojie.cn
 */
 
-var $nobyda = nobyda();
+var $ = new Env('');
 var date = new Date()
-if ($nobyda.isRequest) {
+if (typeof $request != "undefined") {
   GetCookie()
 } else {
   checkin()
 }
 
 function checkin() {
-  var bonus = {
+  $.get({
     url: 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2&mobile=no',
     headers: {
-      Cookie: $nobyda.read("CookieWA"),
+      Cookie: CookieWA || $.getdata("CookieWA"),
     }
-  };
-  $nobyda.get(bonus, function(error, response, data) {
+  }, function(error, response, data) {
     if (error) {
-      console.log(error);
-      $nobyda.notify("吾爱破解", "签到请求失败 ‼️‼️", error)
+      $.log(error);
+      $.msg("吾爱破解", "签到请求失败 ‼️‼️", error)
     } else {
-      if (data.match(/(ÒÑÍê³É|\u606d\u559c\u60a8)/)) {
-        $nobyda.notify("吾爱破解", "", date.getMonth() + 1 + "月" + date.getDate() + "日, 签到成功 🎉")
-      } else if (data.match(/(ÄúÒÑ|\u4e0b\u671f\u518d\u6765)/)) {
-        $nobyda.notify("吾爱破解", "", date.getMonth() + 1 + "月" + date.getDate() + "日, 已签过 ⚠️")
-      } else if (data.match(/(ÏÈµÇÂ¼|\u9700\u8981\u5148\u767b\u5f55)/)) {
-        $nobyda.notify("吾爱破解", "", "签到失败, Cookie失效 ‼️‼️")
+      if (data.match(/(ÒÑÍê³É|\u606d\u559c\u60a8|��̳΢�š��ᰮ�ƽ�)/)) {
+        $.msg("吾爱破解", "", date.getMonth() + 1 + "月" + date.getDate() + "日, 签到成功 🎉")
+      } else if (data.match(/(ÄúÒÑ|\u4e0b\u671f\u518d\u6765|>��Ǹ������)/)) {
+        $.msg("吾爱破解", "", date.getMonth() + 1 + "月" + date.getDate() + "日, 已签过 ⚠️")
+      } else if (data.match(/(ÏÈµÇÂ¼|\u9700\u8981\u5148\u767b\u5f55|�Ҫ�ȵ�¼���ܼ�)/)) {
+        $.msg("吾爱破解", "", "签到失败, Cookie失效 ‼️‼️")
       } else {
-        $nobyda.notify("吾爱破解", "", "脚本待更新 ‼️‼️")
+        $.msg("吾爱破解", "", "脚本待更新 ‼️‼️")
       }
     }
-    $nobyda.done();
+    $.done();
   })
 }
 
@@ -94,83 +105,31 @@ function GetCookie() {
       var CookieName = "吾爱破解";
       var CookieKey = "CookieWA";
       var CookieValue = $request.headers['Cookie'];
-      if ($nobyda.read(CookieKey)) {
-        if ($nobyda.read(CookieKey) != CookieValue) {
-          var cookie = $nobyda.write(CookieValue, CookieKey);
+      if ($.getdata(CookieKey)) {
+        if ($.getdata(CookieKey) != CookieValue) {
+          var cookie = $.setdata(CookieValue, CookieKey);
           if (!cookie) {
-            $nobyda.notify("", "", "更新" + CookieName + "Cookie失败 ‼️");
+            $.msg("", "", "更新" + CookieName + "Cookie失败 ‼️");
           } else {
-            $nobyda.notify("", "", "更新" + CookieName + "Cookie成功 🎉");
+            $.msg("", "", "更新" + CookieName + "Cookie成功 🎉");
           }
         }
       } else {
-        var cookie = $nobyda.write(CookieValue, CookieKey);
+        var cookie = $.setdata(CookieValue, CookieKey);
         if (!cookie) {
-          $nobyda.notify("", "", "首次写入" + CookieName + "Cookie失败 ‼️");
+          $.msg("", "", "首次写入" + CookieName + "Cookie失败 ‼️");
         } else {
-          $nobyda.notify("", "", "首次写入" + CookieName + "Cookie成功 🎉");
+          $.msg("", "", "首次写入" + CookieName + "Cookie成功 🎉");
         }
       }
     } else {
-      $nobyda.notify("写入Cookie失败", "", "请检查匹配URL或配置内脚本类型 ‼️");
+      $.msg("写入Cookie失败", "", "请检查匹配URL或配置内脚本类型 ‼️");
     }
   } catch (eor) {
-    $nobyda.notify("写入Cookie失败", "", "未知错误 ‼️")
-    console.log(JSON.stringify(eor) + "\n" + eor + "\n" + JSON.stringify($request.headers))
+    $.msg("写入Cookie失败", "", "未知错误 ‼️")
+    $.log(JSON.stringify(eor) + "\n" + eor + "\n" + JSON.stringify($request.headers))
   }
-  $nobyda.done();
+  $.done();
 }
-
-function nobyda() {
-  const isRequest = typeof $request != "undefined"
-  const isSurge = typeof $httpClient != "undefined"
-  const isQuanX = typeof $task != "undefined"
-  const notify = (title, subtitle, message) => {
-    if (isQuanX) $notify(title, subtitle, message)
-    if (isSurge) $notification.post(title, subtitle, message)
-  }
-  const write = (value, key) => {
-    if (isQuanX) return $prefs.setValueForKey(value, key)
-    if (isSurge) return $persistentStore.write(value, key)
-  }
-  const read = (key) => {
-    if (isQuanX) return $prefs.valueForKey(key)
-    if (isSurge) return $persistentStore.read(key)
-  }
-  const adapterStatus = (response) => {
-    if (response) {
-      if (response.status) {
-        response["statusCode"] = response.status
-      } else if (response.statusCode) {
-        response["status"] = response.statusCode
-      }
-    }
-    return response
-  }
-  const get = (options, callback) => {
-    if (isQuanX) {
-      if (typeof options == "string") options = {
-        url: options
-      }
-      options["method"] = "GET"
-      $task.fetch(options).then(response => {
-        callback(null, adapterStatus(response), response.body)
-      }, reason => callback(reason.error, null, null))
-    }
-    if (isSurge) $httpClient.get(options, (error, response, body) => {
-      callback(error, adapterStatus(response), body)
-    })
-  }
-  const done = (value = {}) => {
-    if (isQuanX) return $done(value)
-    if (isSurge) isRequest ? $done(value) : $done()
-  }
-  return {
-    isRequest,
-    notify,
-    write,
-    read,
-    get,
-    done
-  }
-};
+//Compatible code from https://github.com/chavyleung/scripts/blob/master/Env.min.js
+function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("","")}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r)));let h=["",""];h.push(e),s&&h.push(s),i&&h.push(i),console.log(h.join("\n")),this.logs=this.logs.concat(h)}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
