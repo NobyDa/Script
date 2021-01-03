5dcedfefe422849bdbf2b271f7571263201f1471
Xirou <54771724+Xirou@users.noreply.github.com>
2021-01-03 19:16:20 +0800

    Add files via upload

---
+++ QuantumultX/VerifyReceipt_Unlock.js
@@ -0,0 +1,14 @@
+//VVEBO
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body vvebo.js
+
+//Videoleap
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body Videoleap.js
+
+//熊掌记
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/verify_receipt.js
+
+//shred
+^https:\/\/api\.shred\.app\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/shred.js
+
+
+
