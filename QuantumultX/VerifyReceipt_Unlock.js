9eab81fc4b27c2258985b040cdb5d5ab83acd793
Xirou <54771724+Xirou@users.noreply.github.com>
2021-01-03 19:56:35 +0800

    Update VerifyReceipt_Unlock.js

--- QuantumultX/VerifyReceipt_Unlock.js
+++ QuantumultX/VerifyReceipt_Unlock.js
@@ -8,7 +8,8 @@
 ^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/verify_receipt.js
 
 //shred
-^https:\/\/api\.shred\.app\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/shred.js
+^https:\/\/api\.shred\.app\/verifyReceipt url script-response-body https://raw.githubusercontent.com/langkhach270389/Scripting/master/shred.js
 
-
+//RCam
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/receipt.js
 
