9ce3f55d8a3533c325e881b29261ac9b88fe3b34
NobyDa <53217160+NobyDa@users.noreply.github.com>
2021-05-31 22:45:01 +0800

    fix #302 #303

--- QuantumultX/File/vsco.js
+++ QuantumultX/File/vsco.js
@@ -1,23 +1,19 @@
 /*
 VSCO unlock vip
+This script is compatible with new and old versions
 
 QX:
-^https?:\/\/vsco\.co\/api\/subscriptions\/2.1\/user-subscriptions\/ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+^https:\/\/(api\.revenuecat\.com\/v\d\/subscribers|vsco\.co\/api\/subscriptions\/\d\.\d\/user-subscriptions)\/ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
 
 Surge4：
-http-response ^https?:\/\/vsco\.co\/api\/subscriptions\/2.1\/user-subscriptions\/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+http-response ^https:\/\/(api\.revenuecat\.com\/v\d\/subscribers|vsco\.co\/api\/subscriptions\/\d\.\d\/user-subscriptions)\/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
 
-Surge & QX MITM = vsco.co
+Surge & QX MITM = vsco.co, api.revenuecat.com
 */
 
-var body = $response.body;
-var url = $request.url;
+let obj = JSON.parse($response.body || '{}');
 
-const path1 = '/api/subscriptions/2.1/user-subscriptions/';
-
-let obj = JSON.parse(body);
-
-if (url.indexOf(path1) != -1) {
+if (obj.user_subscription) {
 	obj.user_subscription["expires_on_sec"] = 1655536094;
 	obj.user_subscription["expired"] = false;
 	obj.user_subscription["payment_type"] = 2;
@@ -29,7 +25,30 @@ if (url.indexOf(path1) != -1) {
 	obj.user_subscription["subscription_code"] = "VSCOANNUAL";
 	obj.user_subscription["user_id"] = 54624336;
 	obj.user_subscription["source"] = 1;
-	body = JSON.stringify(obj);  
- }
+}
 
-$done({body});
+if (obj.subscriber) {
+	obj.subscriber.subscriptions = {
+		"com.circles.fin.premium.yearly": {
+			"billing_issues_detected_at": null,
+			"expires_date": "2030-02-18T07:52:54Z",
+			"is_sandbox": false,
+			"original_purchase_date": "2020-02-11T07:52:55Z",
+			"period_type": "normal",
+			"purchase_date": "2020-02-11T07:52:54Z",
+			"store": "app_store",
+			"unsubscribe_detected_at": null
+		}
+	};
+	obj.subscriber.entitlements = {
+		"membership": {
+			"expires_date": "2030-02-18T07:52:54Z",
+			"product_identifier": "com.circles.fin.premium.yearly",
+			"purchase_date": "2020-02-11T07:52:54Z"
+		}
+	};
+}
+
+$done({
+	body: JSON.stringify(obj)
+});
