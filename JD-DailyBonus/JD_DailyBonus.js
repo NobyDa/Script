2ad8ea2619f7e92a24c1e6f020a615f79f968f8d
bai3950975 <36722783+bai3950975@users.noreply.github.com>
2022-02-10 09:38:03 +0800

    Update JD_DailyBonus.js

--- JD-DailyBonus/JD_DailyBonus.js
+++ JD-DailyBonus/JD_DailyBonus.js
@@ -46,7 +46,9 @@ var Key = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用jso
 
 var DualKey = ''; //该参数已废弃; 仅用于下游脚本的兼容, 请使用json串数据  ↓
 
-var OtherKey = ``; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
+var OtherKey = ` [{
+  "cookie": "pt_key=AAJiBGpwADAKrJrSz8y65N5khUid3efVihKRPGrvWsyXOnBbxSjWDKo5PI8wPakmoPBGrOzl0G4;pt_pin=292873681-137887;"
+}]`; //无限账号Cookie json串数据, 请严格按照json格式填写, 具体格式请看以下样例:
 
 /*以下样例为双账号("cookie"为必须,其他可选), 第一个账号仅包含Cookie, 第二个账号包含Cookie和金融签到Body: 
 
