f489cbbb2ff516543d54b865b53c0afdb293d6a9
NobyDa <53217160+NobyDa@users.noreply.github.com>
2022-07-03 07:41:54 +0800

    Update support 1Blocker in-app purchases.

--- QuantumultX/File/vsco.js
+++ QuantumultX/File/vsco.js
@@ -1,37 +1,52 @@
-/* 
- * Membership unlock for vsco & filebox
- * Please note that you may need to reinstall app to make it work.
- * 
- * Type: http-response (requires body)
- * Regex: ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/
- * Mitm: api.revenuecat.com
- */
+/********************************
+Membership unlock for VSCO & Fileball & 1Blocker
+Please note that you may need to reinstall app for script to work.
+
+QuantumultX rewrite link:
+https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+
+Please note that the above rewrite link requires open KOP-XIAO's resource parser
+
+*********************************
+Surge4, Loon and Shadowrocket configuration:
+
+[Script]
+http-request ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+http-response ^https?:\/\/api\.revenuecat\.com\/v\d\/subscribers\/ requires-body=1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+
+[MITM]
+hostname = api.revenuecat.com
+********************************/
 
 const resp = {};
-const obj = JSON.parse($response.body || null);
-const app = /VSCO|Fileb(ox|all)/.test($request.headers['User-Agent']);
-
-const product = {
-	"membership": "com.circles.fin.premium.yearly", //vsco
-	"filebox_pro": "com.premium.yearly" //filebox
-}
+const obj = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
+const list = {
+	'VSCO': { name: 'membership', id: 'com.circles.fin.premium.yearly' },
+	'Filebox': { name: 'filebox_pro', id: 'com.premium.yearly' },
+	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' },
+	'1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' }
+};
 const data = {
 	"expires_date": "2030-02-18T07:52:54Z",
 	"original_purchase_date": "2020-02-11T07:52:55Z",
 	"purchase_date": "2020-02-11T07:52:54Z"
 };
 
-if (app && obj && obj.subscriber) {
-	if (!obj.subscriber.subscriptions) {
-		obj.subscriber.subscriptions = {};
-	}
-	if (!obj.subscriber.entitlements) {
-		obj.subscriber.entitlements = {};
-	}
-	for (const i in product) {
-		obj.subscriber.subscriptions[product[i]] = data;
-		obj.subscriber.entitlements[i] = data;
-		obj.subscriber.entitlements[i].product_identifier = product[i];
+if (typeof $response == "undefined") {
+	delete $request.headers["x-revenuecat-etag"]; // prevent 304 issues
+	delete $request.headers["X-RevenueCat-ETag"];
+	resp.headers = $request.headers;
+} else if (obj && obj.subscriber) {
+	obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
+	obj.subscriber.entitlement = obj.subscriber.entitlement || {};
+	for (const i in list) {
+		if (new RegExp(`^${i}`, `i`).test(ua)) {
+			obj.subscriber.subscriptions[list[i].id] = data;
+			obj.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+			obj.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+			break;
+		}
 	}
 	resp.body = JSON.stringify(obj);
 }
