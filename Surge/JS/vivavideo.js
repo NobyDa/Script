ac525811d1aa45cee3293be4314ecd43d3b41a25
NobyDa <53217160+NobyDa@users.noreply.github.com>
2022-07-03 10:49:07 +0800

    Update vivavideo script regex

--- Surge/JS/vivavideo.js
+++ Surge/JS/vivavideo.js
@@ -5,19 +5,19 @@
 QuantumultX:
 
 [rewrite_local]
-^https:\/\/xy-viva\.kakalili\.com\/api\/rest\/u\/vipVerifyReceipt url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
 
 [mitm]
-hostname = xy-viva.kakalili.com
+hostname = xy-viva.kakalili.com, api-chn.rthdo.com
 
 ***************************
 Surge4 or Loon:
 
 [Script]
-http-response ^https:\/\/xy-viva\.kakalili\.com\/api\/rest\/u\/vipVerifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+http-response ^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
 
 [MITM]
-hostname = xy-viva.kakalili.com
+hostname = xy-viva.kakalili.com, api-chn.rthdo.com
 
 **************************/
 
